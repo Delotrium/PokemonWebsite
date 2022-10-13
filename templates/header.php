463c07db56438f172ae92be8b7cbc78d243ee2ec
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2022-10-13 15:00:02 +1000

    Add files via upload

---
+++ templates/header.php
@@ -0,0 +1,27 @@
+<head>
+	<title>Pokédex</title>
+	<!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <style type="text/css">
+	  .brand{
+	  	background: #FFDE00 !important;
+	  }
+  	.brand-text{
+  		color: #CC0000 !important;
+  	}
+  	form{
+  		max-width: 460px;
+  		margin: 20px auto;
+  		padding: 20px;
+  	}
+  </style>
+</head>
+<body style="background-color:#e5e10b;">
+	<nav class="white z-depth-0">
+    <div class="container">
+      <a href="index.php" class="brand-logo brand-text">Pokédex</a>
+      <ul id="nav-mobile" class="right hide-on-small-and-down">
+        <li><a href="add.php" class="btn brand z-depth-0">Create an Account</a></li>
+      </ul>
+    </div>
+  </nav>
